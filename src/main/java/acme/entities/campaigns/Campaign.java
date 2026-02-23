658941e75745eb5c35e681b057a9d91781f77c8c
Julio Macias <julmacgon@alum.us.es>
2026-02-23 11:03:47 +0100

    add: @validAnnotations

--- src/main/java/acme/entities/campaigns/Campaign.java
+++ src/main/java/acme/entities/campaigns/Campaign.java
@@ -19,6 +19,9 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoment.Constraint;
 import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidHeader;
+import acme.constraints.ValidText;
+import acme.constraints.ValidTicker;
 import acme.features.campaigns.MilestoneRepository;
 import acme.realms.Spokesperson;
 import lombok.Getter;
@@ -36,17 +39,17 @@ public class Campaign extends AbstractEntity {
 	// Attributes ----------------------------------------------------
 
 	@Mandatory
-	//    @ValidTicker
+	@ValidTicker
 	@Column(unique = true)
 	private String				ticker;
 
 	@Mandatory
-	//    @ValidHeader
+	@ValidHeader
 	@Column
 	private String				name;
 
 	@Mandatory
-	//    @ValidText
+	@ValidText
 	@Column
 	private String				description;
 
