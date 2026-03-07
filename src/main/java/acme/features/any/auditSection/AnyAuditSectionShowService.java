f932597add4df6e5822585a056f03b872bf88ba7
JaimeGarcia2005 <jaigarcar@alum.us.es>
2026-03-07 12:32:39 +0100

    fix: added kinds to the auditSection show

--- src/main/java/acme/features/any/auditSection/AnyAuditSectionShowService.java
+++ src/main/java/acme/features/any/auditSection/AnyAuditSectionShowService.java
@@ -15,9 +15,12 @@ package acme.features.any.auditSection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import acme.client.components.models.Tuple;
 import acme.client.components.principals.Any;
+import acme.client.components.views.SelectChoices;
 import acme.client.services.AbstractService;
 import acme.entities.audits.AuditSection;
+import acme.entities.audits.SectionKind;
 
 @Service
 public class AnyAuditSectionShowService extends AbstractService<Any, AuditSection> {
@@ -51,7 +54,14 @@ public class AnyAuditSectionShowService extends AbstractService<Any, AuditSectio
 
 	@Override
 	public void unbind() {
-		super.unbindObject(this.auditSection, "name", "notes", "hours", "kind");
+
+		SelectChoices choices;
+		Tuple tuple;
+
+		choices = SelectChoices.from(SectionKind.class, this.auditSection.getKind());
+
+		tuple = super.unbindObject(this.auditSection, "name", "notes", "hours", "kind");
+		tuple.put("kinds", choices);
 	}
 
 }
