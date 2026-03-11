0b53559c1a372388c9ed972ef805cdd3380ef607
Franh28 <fraherde@alum.us.es>
2026-03-11 18:08:29 +0100

    feat: backend list & show any

---
+++ src/main/java/acme/features/any/sponsorship/AnySponsorshipController.java
@@ -0,0 +1,37 @@
+/*
+ * AnySponsorshipController.java
+ *
+ * Copyright (C) 2012-2026 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.any.sponsorship;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+
+import acme.client.components.principals.Any;
+import acme.client.controllers.AbstractController;
+import acme.entities.sponsorships.Sponsorship;
+
+@Controller
+public class AnySponsorshipController extends AbstractController<Any, Sponsorship> {
+
+	// Constructors -----------------------------------------------------------
+
+	@PostConstruct
+	protected void initialise() {
+		super.setMediaType(MediaType.TEXT_HTML);
+
+		super.addBasicCommand("list", AnySponsorshipListService.class);
+		super.addBasicCommand("show", AnySponsorshipShowService.class);
+	}
+
+}
---
+++ src/main/java/acme/features/any/sponsorship/AnySponsorshipListService.java
@@ -0,0 +1,52 @@
+/*
+ * AnySponsorshipListService.java
+ *
+ * Copyright (C) 2012-2026 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.any.sponsorship;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.components.principals.Any;
+import acme.client.services.AbstractService;
+import acme.entities.sponsorships.Sponsorship;
+
+@Service
+public class AnySponsorshipListService extends AbstractService<Any, Sponsorship> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AnySponsorshipRepository	repository;
+
+	private Collection<Sponsorship>		sponsorships;
+
+	// AbstractService interface -------------------------------------------
+
+
+	@Override
+	public void load() {
+		this.sponsorships = this.repository.findPublishedSponsorships();
+	}
+
+	@Override
+	public void authorise() {
+		super.setAuthorised(true);
+	}
+
+	@Override
+	public void unbind() {
+		super.unbindObjects(this.sponsorships, "ticker", "name", "description", "startMoment", "endMoment", "sponsor.identity.fullName");
+	}
+
+}
