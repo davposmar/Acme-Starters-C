78a89c98a643a66304ca04f34ddc12f3729737a3
Franh28 <fraherde@alum.us.es>
2026-03-11 20:29:24 +0100

    feat: backend list & show sponsor

---
+++ src/main/java/acme/features/sponsor/sponsorship/SponsorSponsorshipController.java
@@ -0,0 +1,37 @@
+/*
+ * SponsorSponsorshipController.java
+ *
+ * Copyright (C) 2012-2026 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.sponsor.sponsorship;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+
+import acme.client.components.principals.Any;
+import acme.client.controllers.AbstractController;
+import acme.entities.sponsorships.Sponsorship;
+
+@Controller
+public class SponsorSponsorshipController extends AbstractController<Any, Sponsorship> {
+
+	// Constructors -----------------------------------------------------------
+
+	@PostConstruct
+	protected void initialise() {
+		super.setMediaType(MediaType.TEXT_HTML);
+
+		super.addBasicCommand("list", SponsorSponsorshipListService.class);
+		super.addBasicCommand("show", SponsorSponsorshipShowService.class);
+	}
+
+}
